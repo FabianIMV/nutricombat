48791e04ccabb494eb8ac527f0c1658e429d99c4
FabianIMV <fabianignaciomv@gmail.com>
2025-09-16 11:54:11 -0300

    se limpia repo para nueva arquitectura

--- backend/src/health/health.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { HealthController } from './health.controller';
-import { SupabaseService } from '../supabase/supabase.service';
-
-@Module({
-  controllers: [HealthController],
-  providers: [SupabaseService],
-})
-export class HealthModule {}
